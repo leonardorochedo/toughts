58d998958f36fb92b33bbeecf9c8cd5d69ef7139
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-10-31 15:51:42 -0300

    create error message password not matches

--- controllers/AuthCountroller.js
+++ controllers/AuthCountroller.js
@@ -1,3 +1,9 @@
+// import dele para manipular o banco
+const User = require("../models/User")
+
+// encriptar senha
+const bcrypt = require("bcryptjs")
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login')
@@ -5,5 +11,19 @@ module.exports = class AuthController {
 
     static register(req, res) {
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res) {
+        
+        const {name, email, password, confirmpassword} = req.body
+
+        // password match validation
+        if(password != confirmpassword) {
+            // mensagem de erro
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
+
+            return
+        }
     }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,6 @@ const AuthController = require("../controllers/AuthCountroller")
 
 router.get('/login', AuthController.login)
 router.get('/register', AuthController.register)
+router.post('/register', AuthController.registerPost)
 
 module.exports = router
