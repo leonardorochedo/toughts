e9f213de1db28c98660b0f40f118c671c5f74e9d
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-11-01 14:39:46 -0300

    create tought route and back-end finish

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -10,4 +10,31 @@ module.exports = class ToughtsController {
     static async dashboard(req, res) {
         res.render('toughts/dashboard')
     }
+
+    static createTought(req, res) {
+        res.render('toughts/create')
+    }
+
+    static async createToughtSave(req, res) {
+
+        // criando o objeto que iremos inserir no banco
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid // pegando o id do user na session
+        }
+
+        try {
+            await Tought.create(tought) // criando pensamento
+
+            req.flash('message', 'Pensamento criado com sucesso!')
+
+            // salvando a sessao e redirecionando
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch(err) {
+            console.log(err)
+        }
+        
+    }
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -7,6 +7,9 @@ const ToughtsController = require("../controllers/ToughtsController")
 // helper / middleware de checkauth
 const checkAuth = require("../helpers/auth").checkAuth
 
+router.get('/add', checkAuth, ToughtsController.createTought) // 2 arguymento de validacao de rota
+router.post('/add', checkAuth, ToughtsController.createToughtSave) // 2 arguymento de validacao de rota
+
 router.get('/dashboard', checkAuth, ToughtsController.dashboard) // 2 arguymento de validacao de rota
 router.get('/', ToughtsController.showToughts)
 
